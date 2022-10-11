330f0a5a5f98f0e4cec67ff2144cccd2a45c9df3
it20611606 <88665626+Siraj125@users.noreply.github.com>
2022-10-11 15:10:16 +0000

    first launch

---
+++ main.py
@@ -0,0 +1,57 @@
+import discord
+import random
+from replit import db
+from keep_alive import keep_alive
+
+intent = discord.Intents.default()
+intent.members = True
+intent.message_content = True
+
+client = discord.Client(intents=intent)
+
+if "responding" not in db.keys():
+  db["responding"] = True
+
+summon_words = ["hi siraj","hey siraj","wodup siraj"]
+
+starter_greetings = [
+  "lol hi", "Hi there", "run", "you dont belong here", "hello bossa",
+  "Whatre you doing here???", "wodup nibba", "nigga mans, sup?", "sup?",
+  "wodup homeboi"
+]
+
+
+@client.event
+async def on_ready():
+  print("We have logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$hello'):
+    await message.channel.send("stfu nigga")
+
+  if message.content.startswith('siraj pls curse'):
+    await message.channel.send("fuck you you piece of fucking shit head")
+
+  if db["responding"]:
+    if any(word in message.content for word in summon_words):
+      await message.channel.send(random.choice(starter_greetings))
+
+  if message.content.startswith('$responding'):
+    value = message.content.split("$responding ", 1)[1]
+
+    if value.lower() == "true":
+      db["responding"] = True
+      await message.channel.send("Heheheh thanks for the unmute xD")
+    if value.lower() == "false":
+      db["responding"] = False
+      await message.channel.send("I will now stfu")
+
+
+keep_alive()
+client.run(
+  'MTAyOTI0OTA3ODc2NjM1ODU5OQ.GOTpkR.aEw_CkeKVBkAH1pQBnPGZuR5ActjemIzZmILGw')
